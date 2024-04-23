e71a463595514a937d2a077bdb1b58975e01c325
Mohamed Hussein <150954700+MohamedHussein811@users.noreply.github.com>
2024-04-23 14:45:49 +0200

    0.1

---
+++ client/src/components/objectDetector/index.tsx
@@ -0,0 +1,51 @@
+import React, { useRef, useState, useEffect } from "react";
+import axios from "axios"; 
+
+interface Prediction {
+  bbox: number[];
+  class: string;
+  score: number;
+}
+
+export function ObjectDetector() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [predictions, setPredictions] = useState<Prediction[]>([]);
+
+  const onSelectImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files && e.target.files[0];
+    if (!file) return;
+
+    try {
+      const formData = new FormData();
+      formData.append("image", file);
+
+      const response = await axios.post("http://localhost:5000/upload", formData);
+      
+      const data = response.data;
+      console.log("Predictions:", data.predictions);
+      setPredictions(data.predictions);
+    } catch (error) {
+      console.error("Error occurred while fetching predictions:", error);
+      setPredictions([]);
+    }
+  };
+
+  useEffect(() => {
+    console.log("Updated Predictions:", predictions);
+  }, [predictions]);
+
+  return (
+    <div>
+      <div>
+        {predictions.length > 0 && (
+          <ul>
+            {predictions.map((prediction, index) => (
+              <li key={index}>{`${prediction.class} - Score: ${prediction.score}`}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <input type="file" ref={fileInputRef} onChange={onSelectImage} />
+    </div>
+  );
+}
---
+++ server/index.js
@@ -0,0 +1,86 @@
+// index.js
+const express = require("express");
+const multer = require("multer");
+require("@tensorflow-models/coco-ssd");
+require("@tensorflow/tfjs-backend-cpu");
+const cors = require("cors");
+const sharp = require("sharp");
+const tf = require("@tensorflow/tfjs");
+
+const cocoSsd = require("@tensorflow-models/coco-ssd");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const allowedOrigins = [
+  "https://novabot.vercel.app", // Front-End Production
+  "http://localhost:5173", // Front-End Localhost
+];
+
+// Middlewares
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["POST", "GET", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+// Multer configuration for handling image uploads
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+  fileFilter: (req, file, cb) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Only image files are allowed"));
+    }
+    cb(null, true);
+  },
+});
+
+let model;
+
+async function loadModel() {
+  try {
+    model = await cocoSsd.load({});
+
+    console.log("Model loaded");
+  } catch (err) {
+    console.error("Failed to load model", err);
+    process.exit(1);
+  }
+}
+loadModel();
+
+app.post("/upload", upload.single("image"), async (req, res) => {
+  try {
+    const imageBuffer = req.file.buffer;
+    if (!model) {
+      return res.status(500).send("Model not loaded yet");
+    }
+
+    // Convert image buffer to tensor
+    const imageTensor = await sharp(imageBuffer)
+      .resize(300)
+      .raw()
+      .toBuffer({ resolveWithObject: true })
+      .then(({ data, info }) => {
+        return tf.tensor3d(data, [info.height, info.width, info.channels]);
+      });
+
+    const predictions = await model.detect(imageTensor);
+
+    // Send predictions back to the client
+    res.json({ predictions });
+  } catch (error) {
+    console.error("Error occurred during object detection:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
